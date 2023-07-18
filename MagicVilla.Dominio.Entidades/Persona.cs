580807891e664093a6eb5c553278189be8b9222a
Dianet Utria <dia.utria@gmail.com>
2023-07-18 00:08:35 -0300

    Agregando set a propiedade Sexo en la entidad Persona

--- MagicVilla.Dominio.Entidades/Persona.cs
+++ MagicVilla.Dominio.Entidades/Persona.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellidos { get; set;}
-        public string Sexo { get;}
+        public string Sexo { get; set; }
 
         protected Persona()
         {
